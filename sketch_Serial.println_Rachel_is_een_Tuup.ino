3d8ebdab88ab827255b726ee49686df8361d232c
theOracle1984 <36340180+theOracle1984@users.noreply.github.com>
2018-02-10 19:49:53 +0100

    Add files via upload
    hahaha :p

---
+++ sketch_Serial.println_Rachel_is_een_Tuup.ino
@@ -0,0 +1,16 @@
+//A variable to hold the level of coolness
+int coolFactor = 3;
+
+void setup() {
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  //Send the value of coolFactor to the the Serial port.
+  //So we can see it in the serial monitor window
+  Serial.print(coolFactor);
+
+}
